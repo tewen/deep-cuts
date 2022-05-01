30111c1ba12fbdaf653248e77d06d7f45fe443cd
Trevor Ewen <trevor@stg.software>
2022-05-01 07:42:59 -0400

    keyValuePairs method, upgrade development node version. (#30)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,14 @@ export { csvRowsToObjects, objectsToCsvRows } from './csv';
 export { acceptNoArguments, functionOrValue, tryCatch } from './function';
 export { isJsonString, safeJsonParse } from './json';
 export { parseFloatOrUndefined, parseIntegerOrUndefined } from './number';
-export { isNil, isEmpty, isObject, merge, flattenObject } from './object';
+export {
+  isNil,
+  isEmpty,
+  isObject,
+  keyValuePairs,
+  merge,
+  flattenObject,
+} from './object';
 export { jsonStreamToObject } from './stream';
 export {
   cleanSpecialCharacters,

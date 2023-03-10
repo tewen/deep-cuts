2ed4a51ccaa16a6e950837b67a601c3eba1d0b37
Trevor Ewen <trevor@southporttechnologygroup.com>
2023-03-09 22:10:07 -0500

    Round to nearest fraction method. (#37)

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export {
   currencyToFloat,
   parseFloatOrUndefined,
   parseIntegerOrUndefined,
+  roundToNearestFraction,
 } from './number';
 export {
   isNil,
--- src/number.ts
+++ src/number.ts
@@ -1,3 +1,5 @@
+import { isNil } from './object';
+
 function isDecimalOrInteger(value: any): boolean {
   return /^[-+]?[0-9]+(\.[0-9]+|)$/.test(String(value));
 }
@@ -12,11 +14,30 @@ export function parseIntegerOrUndefined(value: any): number | undefined {
   return isDecimalOrInteger(value) && !isNaN(parsed) ? parsed : undefined;
 }
 
-export function currencyToFloat(currency: string | number, sign: string = '$') {
+export function currencyToFloat(
+  currency: string | number,
+  sign: string = '$'
+): number | undefined {
   return parseFloatOrUndefined(
     (String(currency) || '')
       .replace(new RegExp('^\\' + sign, 'ig'), '')
       .replace(/,/gi, '')
       .trim()
   );
+}
+
+export function roundToNearestFraction(
+  value: string | number,
+  denominator: number,
+  maxDecimalPlaces: number
+): number | undefined {
+  const valueOrUndefined = parseFloatOrUndefined(value);
+  if (!isNil(valueOrUndefined)) {
+    return parseFloat(
+      (
+        Math.round((valueOrUndefined as number) * denominator) / denominator
+      ).toFixed(maxDecimalPlaces)
+    );
+  }
+  return undefined;
 }

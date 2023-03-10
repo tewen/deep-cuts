ac651f7627a51c5fb081504e99678abc7d40dbca
Trevor Ewen <trevor@southporttechnologygroup.com>
2023-03-09 22:07:58 -0500

    Round to nearest fraction method.

--- src/__tests__/number.test.ts
+++ src/__tests__/number.test.ts
@@ -2,6 +2,7 @@ import {
   parseFloatOrUndefined,
   parseIntegerOrUndefined,
   currencyToFloat,
+  roundToNearestFraction,
 } from '../';
 
 describe('number', () => {
@@ -124,6 +125,94 @@ describe('number', () => {
 
     it('should not worry about decimal places', () => {
       expect(currencyToFloat('$102,455,289.7401')).toBe(102455289.7401);
+    });
+  });
+
+  describe('roundToNearestFraction()', () => {
+    it('should return undefined if passed undefined', () => {
+      // @ts-ignore
+      expect(roundToNearestFraction(undefined, 4, 2)).toBeUndefined();
+    });
+
+    it('should return null if passed null', () => {
+      // @ts-ignore
+      expect(roundToNearestFraction(null, 4, 2)).toBeUndefined();
+    });
+
+    it('should return 0 if passed 0 in any case', () => {
+      expect(roundToNearestFraction(0, 8, 4)).toBe(0);
+    });
+
+    it('should return 0 for a stringified 0', () => {
+      expect(roundToNearestFraction('0', 8, 4)).toBe(0);
+    });
+
+    it('should return a value rounded to the nearest integer if provided 1 for the denominator', () => {
+      expect(roundToNearestFraction(25.6278, 1, 4)).toBe(26);
+    });
+
+    it('should play nice with negative numbers when 1 is the denominator', () => {
+      expect(roundToNearestFraction(-108.87, 1, 2)).toBe(-109);
+    });
+
+    it('should play nice with the string versions of 1 in the denominator, negative and positive', () => {
+      expect(roundToNearestFraction('25.6278', 1, 4)).toBe(26);
+      expect(roundToNearestFraction('-108.87', 1, 2)).toBe(-109);
+    });
+
+    it('should round to the nearest half', () => {
+      expect(roundToNearestFraction(11.2, 2, 4)).toBe(11);
+      expect(roundToNearestFraction(11.4, 2, 4)).toBe(11.5);
+    });
+
+    it('should play nice with negatives for the half', () => {
+      expect(roundToNearestFraction(-163.108, 2, 4)).toBe(-163);
+      expect(roundToNearestFraction(-163.260001, 2, 4)).toBe(-163.5);
+    });
+
+    it('should play nice with the string versions of 2 in the denominator, negative and positive', () => {
+      expect(roundToNearestFraction('11.2', 2, 4)).toBe(11);
+      expect(roundToNearestFraction('11.4', 2, 4)).toBe(11.5);
+      expect(roundToNearestFraction('-163.108', 2, 4)).toBe(-163);
+      expect(roundToNearestFraction('-163.260001', 2, 4)).toBe(-163.5);
+    });
+
+    it('should round to the nearest quarter', () => {
+      expect(roundToNearestFraction(1456.27, 4, 2)).toBe(1456.25);
+      expect(roundToNearestFraction(1456.47, 4, 2)).toBe(1456.5);
+    });
+
+    it('should play nice with negatives for the quarter', () => {
+      expect(roundToNearestFraction(-2.0000001, 4, 2)).toBe(-2);
+      expect(roundToNearestFraction(-2.249999999999, 4, 2)).toBe(-2.25);
+    });
+
+    it('should play nice with the string versions of 4 in the denominator, negative and positive', () => {
+      expect(roundToNearestFraction('1456.27', 4, 2)).toBe(1456.25);
+      expect(roundToNearestFraction('1456.47', 4, 2)).toBe(1456.5);
+      expect(roundToNearestFraction('-2.0000001', 4, 2)).toBe(-2);
+      expect(roundToNearestFraction('-2.249999999999', 4, 2)).toBe(-2.25);
+    });
+
+    it('should round to the nearest hundredth', () => {
+      expect(roundToNearestFraction(20.00501, 100, 3)).toBe(20.01);
+      expect(roundToNearestFraction(20.004099999, 100, 3)).toBe(20);
+    });
+
+    it('should play nice with negatives for the hundredth', () => {
+      expect(roundToNearestFraction(-47.4850000001, 100, 3)).toBe(-47.49);
+      expect(roundToNearestFraction(-47.481, 100, 3)).toBe(-47.48);
+    });
+
+    it('should play nice with the string versions of 100 in the denominator, negative and positive', () => {
+      expect(roundToNearestFraction('20.00501', 100, 3)).toBe(20.01);
+      expect(roundToNearestFraction('20.004099999', 100, 3)).toBe(20);
+      expect(roundToNearestFraction('-47.4850000001', 100, 3)).toBe(-47.49);
+      expect(roundToNearestFraction('-47.481', 100, 3)).toBe(-47.48);
+    });
+
+    it('should be able to cut off decimals with the max decimals', () => {
+      expect(roundToNearestFraction(-47.4850000001, 100, 1)).toBe(-47.5);
     });
   });
 });
